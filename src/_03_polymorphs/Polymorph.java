50aa51b9fb7394490e7700ac86109c0e69bdd1fe
lonedragon513 <lonedragon513@192.168.3.122>
2020-10-24 17:28:29 -0700

    a

--- src/_03_polymorphs/Polymorph.java
+++ src/_03_polymorphs/Polymorph.java
@@ -3,14 +3,46 @@ package _03_polymorphs;
 import java.awt.Graphics;
 
 public abstract class Polymorph {
-    int x;
-    int y;
+    private int x;
+    private int y;
+    private int width;
+    private int height;;
     
     Polymorph(int x, int y){
    	 this.x = x;
    	 this.y = y;
     }
+    int getX() {
+		return x;
+	}
     
+    int getY() {
+		return y;
+	}
+    
+    void setX(int x) {
+		this.x = x;
+	}
+    
+    void setY(int y) {
+		this.y = y;
+	}
+    
+    int getWidth() {
+		return width;
+	}
+    
+    void setWidth(int width) {
+		this.width = width;
+	}
+    
+    int getHeight() {
+		return height;
+	}
+    
+    void setHeight(int height) {
+		this.height = height;
+	}
     public void update(){
    	 
     }
